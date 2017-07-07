f796daacdd860d74b039f210cd6e0b81fa42e239
azizulBakar <azizul_sb@yahoo.com>
2017-07-07 12:17:39 +0800

    update

---
+++ docs/api/get/get-zone.php
@@ -0,0 +1,45 @@
+<?php
+$objFilter = (object) $_GET;
+unset($_GET);
+
+$state='';
+
+if(isset($objFilter->state)){
+  $state = $objFilter->state;
+  $state = str_replace(' ', '+', $state);
+}
+
+if($state!=''){
+
+  $curl = curl_init();
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => "http://api.azanpro.com/zone/grouped.json?state=$state",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
+      "cache-control: no-cache",
+      "postman-token: b3e7690c-4a8d-893c-251f-235f19347602"
+    ),
+  ));
+
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+
+  curl_close($curl);
+
+  if ($err) {
+    $err = array('status' => 'error');
+    echo $errJson = json_encode($err);
+  } else {
+    echo $response;
+  }
+
+}else{
+  $err = array('status' => 'data invalid');
+  echo $errJson = json_encode($err);
+}
