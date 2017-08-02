c19dc60bda26af49ffab129d810be29a76512969
azizulBakar <azizul_sb@yahoo.com>
2017-08-02 16:41:31 +0800

    update

---
+++ app/scripts/directives/advertisement.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name waktusolatmyApp.directive:advertisement
+ * @description
+ * # advertisement
+ */
+angular.module('waktusolatmyApp')
+  .component('advertisement', {
+    templateUrl: 'views/advertisement.html',
+    controller: ['$log',
+      function advertisement($log) {
+        var $ctrl = this;
+
+
+      }]
+  });
